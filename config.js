35434d80a904f406db273046a69c41ae3af9850b
DEMO-IIIRN <demoiiirnstudio@gmail.com>
2025-03-09 14:01:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_MEMBER_URL = 'XXXXXXXX';
-const WEB_APP_URL = 'XXXXXXXX';
-const WEB_APP_Leave_URL = 'XXXXXXXX';
-const WEB_APP_OT_URL = 'XXXXXXXX';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwJWL5mTVRSWP9m6qJT0TSnqJVWG8z0OpeIZHrxyJIZmALhHyiPOCtnHxb9bQVWvPsI/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwjA-zZzP5ICCrpwLwxIhGuAogp2gTGRmBGj4Eu2VWNHEzcfXYug9nq4X1IPe0Lqe-cSg/exec';
+const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbw6ULifAR_ANqGfdh5T1jiG-_PIgNgp7QbV0Co7Y8LgtuIk_hNIVwM2h2fBgrYJK_A/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycby7BGLLwjPtRra10N66tQN9iw7ux51IYYBCWB6qXf3KMl1M8DsPjaZJl9hpM-IQ-HjZnA/exec';
 
 //LIFF_ID
 const LIFF_ID = 'XXXXXXXX'; // LIFF ID Checkin
